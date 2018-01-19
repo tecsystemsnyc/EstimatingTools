0f6e97e55c8310ea362962900fb32c5a9dbc88a8
dtaylor1701 <dktaylor1701@gmail.com>
2018-01-19 16:50:36 -0500

    Futzed with review view and added bidParametersView

---
+++ TECUserControlLibrary/ViewModels/BidParametersVM.cs
@@ -0,0 +1,36 @@
+﻿using EstimatingLibrary;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibrary.ViewModels
+{
+    public class BidParametersVM : ViewModelBase
+    {
+
+        private TECBid _bid;
+
+        public TECBid Bid
+        {
+            get { return _bid; }
+            set
+            {
+                _bid = value;
+                RaisePropertyChanged("Bid");
+            }
+        }
+
+        public BidParametersVM(TECBid bid)
+        {
+            Bid = bid;
+        }
+
+        public void Refresh(TECBid bid)
+        {
+            Bid = bid;
+        }
+    }
+}
