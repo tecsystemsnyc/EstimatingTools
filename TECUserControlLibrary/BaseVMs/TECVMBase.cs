b712c7ebecfae8ba6dd538cb32543279ae8f86b9
Gregory <ghanson@TEC-system.com>
2018-02-26 11:48:11 -0500

    Updated new BaseVM class

--- TECUserControlLibrary/BaseVMs/TECVMBase.cs
+++ TECUserControlLibrary/BaseVMs/TECVMBase.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using EstimatingLibrary;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +8,18 @@ using System.Threading.Tasks;
 
 namespace TECUserControlLibrary.BaseVMs
 {
-    public abstract class HasModalVM : ViewModelBase
+    public abstract class TECVMBase : ViewModelBase
     {
         public event Action<ViewModelBase> ModalVMStarted;
+        public event Action<TECObject> ObjectSelected;
 
         protected void StartModal(ViewModelBase vm)
         {
             ModalVMStarted?.Invoke(vm);
+        }
+        protected void RaiseSelectedChanged(TECObject obj)
+        {
+            ObjectSelected?.Invoke(obj);
         }
     }
 }
