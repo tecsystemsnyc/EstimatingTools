bd01e87a4df8a58c3647395c8d1f4117b91069e5
Gregory <ghanson@TEC-system.com>
2018-02-26 11:00:01 -0500

    Added HasModalVM base class

---
+++ TECUserControlLibrary/BaseVMs/HasModalVM.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibrary.BaseVMs
+{
+    public abstract class HasModalVM : ViewModelBase
+    {
+        public event Action<ViewModelBase> ModalVMStarted;
+
+        protected void StartModal(ViewModelBase vm)
+        {
+            ModalVMStarted?.Invoke(vm);
+        }
+    }
+}
