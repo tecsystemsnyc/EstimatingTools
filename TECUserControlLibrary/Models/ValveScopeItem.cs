f6100189e8e635cbeaf65fbeafd113a86d6a6df4
dtaylor1701 <dktaylor1701@gmail.com>
2018-03-14 17:03:24 -0400

    Fleshing out controls for valve selection

---
+++ TECUserControlLibrary/Models/ValveScopeItem.cs
@@ -0,0 +1,23 @@
+﻿using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibrary.Models
+{
+    public class ValveScopeItem
+    {
+        public TECEquipment Equipment { get; }
+        public TECSubScope SubScope { get; }
+        public TECValve Valve { get; }
+
+        public ValveScopeItem(TECEquipment equipment, TECSubScope subScope, TECValve valve)
+        {
+            this.Equipment = equipment;
+            this.SubScope = subScope;
+            this.Valve = valve;
+        }
+    }
+}
--- TECUserControlLibrary/ViewModels/ValveSelectionVM.cs
+++ TECUserControlLibrary/ViewModels/ValveSelectionVM.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using EstimatingLibrary;
+using EstimatingLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TECUserControlLibrary.Models;
 
 namespace TECUserControlLibrary.ViewModels
 {
     public class ValveSelectionVM
     {
+        public List<TECValve> Catalog { get; }
+        public List<ValveScopeItem> Valves { get; }
+
+        public ValveSelectionVM(TECSystem system, IEnumerable<TECValve> valveCatalog)
+        {
+            Catalog = new List<TECValve>(valveCatalog);
+            Valves = getValveItems(system);
+        }
+
+        private List<ValveScopeItem> getValveItems(TECSystem system)
+        {
+            List<ValveScopeItem> valveItems = new List<ValveScopeItem>();
+            foreach(TECEquipment equipment in system.Equipment)
+            {
+                foreach(TECSubScope subScope in equipment.SubScope)
+                {
+                    foreach(IEndDevice device in subScope.Devices)
+                    {
+                        if(device is TECValve valve)
+                        {
+                            valveItems.Add(new ValveScopeItem(equipment, subScope, valve));
+                        }
+                    }
+                }
+            }
+            return valveItems;
+        }
     }
 }
