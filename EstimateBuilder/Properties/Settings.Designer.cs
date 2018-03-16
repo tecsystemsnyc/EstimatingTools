a6ca452c5112cc9cde60a9ee8ecf933f8737051a
Gregory <ghanson@TEC-system.com>
2018-03-16 15:36:33 -0400

    Added OpenFileOnExport setting and Settings menu item

--- EstimateBuilder/Properties/Settings.Designer.cs
+++ EstimateBuilder/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace EstimateBuilder.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,18 @@ namespace EstimateBuilder.Properties {
             }
             set {
                 this["StartUpFilePath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool OpenFileOnExport {
+            get {
+                return ((bool)(this["OpenFileOnExport"]));
+            }
+            set {
+                this["OpenFileOnExport"] = value;
             }
         }
     }
