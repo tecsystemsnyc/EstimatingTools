f7bdef061ce80cccda29fc91f7a0415e56a45337
dtaylor1701 <dktaylor1701@gmail.com>
2018-03-23 16:25:41 -0400

    Fixed test load data

--- EstimatingUtilitiesLibraryTests/TestDBHelper.cs
+++ EstimatingUtilitiesLibraryTests/TestDBHelper.cs
@@ -211,6 +211,8 @@ namespace EstimatingUtilitiesLibraryTests
             values.Add("0");
             values.Add("0.05");
             values.Add("0.03");
+            values.Add("1.5");
+
             values.Add("0.0875");
             values.Add("0.05");
             values.Add("0.03");
@@ -237,6 +239,7 @@ namespace EstimatingUtilitiesLibraryTests
             values.Add("60");
             values.Add("30");
             values.Add("40");
+
             values.Add("0.25");
             values.Add("0");
             values.Add("1");
