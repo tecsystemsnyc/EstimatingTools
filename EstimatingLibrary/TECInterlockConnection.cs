e71fb0f6047603c06d77cc07ee8d28aed488e2ab
Gregory <ghanson@TEC-system.com>
2018-05-24 16:58:30 -0400

    Composed TECInterlockConnection

--- EstimatingLibrary/TECInterlockConnection.cs
+++ EstimatingLibrary/TECInterlockConnection.cs
@@ -1,4 +1,5 @@
 ﻿using EstimatingLibrary.Interfaces;
+using EstimatingLibrary.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,19 +8,29 @@ using System.Threading.Tasks;
 
 namespace EstimatingLibrary
 {
-    public class TECInterlockConnection : TECScope, IConnection
+    public class TECInterlockConnection : TECScope, IConnection, INotifyCostChanged, IRelatable
     {
-        private TECConnection connection;
+        private readonly TECConnection connection;
         
         public TECInterlockConnection(Guid guid, IProtocol protocol, bool isTypical) : base(guid)
         {
             this.connection = new TECConnection(guid, protocol, isTypical);
+            subscribeToConnection();
         }
+
         public TECInterlockConnection(IProtocol protocol, bool isTypical) : this(Guid.NewGuid(), protocol, isTypical)
         { }
         public TECInterlockConnection(TECInterlockConnection source, bool isTypical, Dictionary<Guid, Guid> guidDictionary = null) : base(source.Guid)
         {
             this.connection = new TECConnection(source.connection, isTypical, guidDictionary);
+            subscribeToConnection();
+        }
+
+        private void subscribeToConnection()
+        {
+            this.connection.CostChanged += notifyCostChanged;
+            this.connection.TECChanged += (args) => notifyTECChanged(args.Change, args.PropertyName, args.Sender, args.Value, args.OldValue);
+            this.connection.PropertyChanged += (sender, args) => raisePropertyChanged(args.PropertyName);
         }
 
         #region IConnection
@@ -29,6 +40,32 @@ namespace EstimatingLibrary
         public double Length { get => ((IConnection)connection).Length; set => ((IConnection)connection).Length = value; }
 
         public IProtocol Protocol => ((IConnection)connection).Protocol;
+        #endregion
+
+        #region INotifyCostChanged
+        protected override CostBatch getCosts()
+        {
+            CostBatch costs = base.getCosts();
+            costs += connection.CostBatch;
+            return costs;
+        }
+        #endregion
+
+        #region IRelatable
+        protected override SaveableMap propertyObjects()
+        {
+            SaveableMap saveList = new SaveableMap();
+            saveList.AddRange(base.propertyObjects());
+            saveList.AddRange(connection.PropertyObjects);
+            return saveList;
+        }
+        protected override SaveableMap linkedObjects()
+        {
+            SaveableMap saveList = new SaveableMap();
+            saveList.AddRange(base.propertyObjects());
+            saveList.AddRange(connection.PropertyObjects);
+            return saveList;
+        }
         #endregion
     }
 }
