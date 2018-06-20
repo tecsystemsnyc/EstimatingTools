2a74cacd6457c5fcbc3d73ca5fa0224fd2a74459
Gregory Hanson <ghanson@tec-system.com>
2018-06-20 10:21:32 -0400

    Added class for extensions to the Random class

---
+++ TestLibrary/RandomExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public static class RandomExtensions
+    {
+        public static void RepeatAction(this Random rand, Action act, int maxTimes = 0)
+        {
+            int numTimes = maxTimes < 0 ? rand.Next() : rand.Next(maxTimes);
+            for(int i = 0; i < numTimes; i++)
+            {
+                act.Invoke();
+            }
+        }
+    }
+}
