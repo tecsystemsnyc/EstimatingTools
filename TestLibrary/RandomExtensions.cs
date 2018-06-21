8083c1e4467248ff4d59ba65c88a4bce70c25fc4
Gregory Hanson <ghanson@tec-system.com>
2018-06-21 14:00:16 -0400

    Added a summary to RepeatAction

--- TestLibrary/RandomExtensions.cs
+++ TestLibrary/RandomExtensions.cs
@@ -8,7 +8,13 @@ namespace TestLibrary
 {
     public static class RandomExtensions
     {
-        public static void RepeatAction(this Random rand, Action act, int maxTimes = 0)
+        /// <summary>
+        /// Does an action a random number of times.
+        /// </summary>
+        /// <param name="rand"></param>
+        /// <param name="act"></param>
+        /// <param name="maxTimes">Defines a maximum number of times the action should be allowed. A negative integer will use Random.Next()</param>
+        public static void RepeatAction(this Random rand, Action act, int maxTimes = -1)
         {
             int numTimes = maxTimes < 0 ? rand.Next() : rand.Next(maxTimes);
             for(int i = 0; i < numTimes; i++)
