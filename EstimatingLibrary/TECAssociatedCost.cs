992b789e595b9010bd8c3c3d469f07e894a8bde0
David Taylor <dktaylor1701@gmail.com>
2018-06-26 11:28:49 -0400

    Drag dropping associated costs now works

--- EstimatingLibrary/TECAssociatedCost.cs
+++ EstimatingLibrary/TECAssociatedCost.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EstimatingLibrary
 {
-    public class TECAssociatedCost : TECCost, ICatalog<TECAssociatedCost>
+    public class TECAssociatedCost : TECCost, ICatalog<TECAssociatedCost>, IDragDropable
     {
         //Constructors built to match TECCost. TECAssociated cost broken out to be own class that implements ICatalog
         public TECAssociatedCost(Guid guid, CostType type) : base(guid, type) { }
@@ -17,6 +17,11 @@ namespace EstimatingLibrary
         public TECAssociatedCost CatalogCopy()
         {
             return new TECAssociatedCost(this);
+        }
+
+        public object DragDropCopy(TECScopeManager scopeManager)
+        {
+            return this;
         }
     }
 }
