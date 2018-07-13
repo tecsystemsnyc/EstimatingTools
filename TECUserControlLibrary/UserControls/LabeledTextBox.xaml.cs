59031dea57570088505d9e338d79d17625bac4f7
David Taylor <dktaylor1701@gmail.com>
2018-07-13 10:52:16 -0400

    Added control for labeledtextbox

---
+++ TECUserControlLibrary/UserControls/LabeledTextBox.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TECUserControlLibrary.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LabeledTextBox.xaml
+    /// </summary>
+    public partial class LabeledTextBox : UserControl
+    {
+
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(LabeledTextBox), new PropertyMetadata("Label"));
+
+
+
+
+        public string InputText
+        {
+            get { return (string)GetValue(InputTextProperty); }
+            set { SetValue(InputTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextBoxText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InputTextProperty =
+            DependencyProperty.Register("InputText", typeof(string), typeof(LabeledTextBox), new PropertyMetadata(""));
+
+
+
+        public double InputWidth
+        {
+            get { return (double)GetValue(InputWidthProperty); }
+            set { SetValue(InputWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InputWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InputWidthProperty =
+            DependencyProperty.Register("InputWidth", typeof(double), typeof(LabeledTextBox), new PropertyMetadata(40.0));
+        
+        public LabeledTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
