0f6cf687fd8d6bb35773b5fa2a91b7bcb3d09527
Gregory Hanson <ghanson@tec-system.com>
2018-07-27 11:15:25 -0400

    Added CatalogContainer

---
+++ EstimatingLibrary/Interfaces/ICatalogContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstimatingLibrary.Interfaces
+{
+    public interface ICatalogContainer : ITECObject
+    {
+        bool ContainsCatalogItem<T>(T item) where T : class, ICatalog<T>;
+        void RemoveCatalogItem<T>(T item, T replacement = null) where T : class, ICatalog<T>;
+    }
+}
