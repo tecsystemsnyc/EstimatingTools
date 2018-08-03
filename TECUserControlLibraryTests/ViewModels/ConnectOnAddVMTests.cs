5752ce451cc0f0372bb77c0b92da5b63168ed687
David Taylor <dktaylor1701@gmail.com>
2018-08-03 13:12:55 -0400

    Added some viewmodel test classes

---
+++ TECUserControlLibraryTests/ViewModels/ConnectOnAddVMTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TECUserControlLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    [TestClass()]
+    public class ConnectOnAddVMTests
+    {
+        [TestMethod()]
+        public void ConnectOnAddVMTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteConnectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteConnectionTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CanConnectTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TECUserControlLibraryTests/ViewModels/ConnectionsVMTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TECUserControlLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    [TestClass()]
+    public class ConnectionsVMTests
+    {
+        [TestMethod()]
+        public void ConnectionsVMTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DragOverTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DropTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
