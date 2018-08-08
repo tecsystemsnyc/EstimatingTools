5e4c05509ae734cb6fb9ce47a2a3e14675834f65
Gregory Hanson <gregorydhanson117@gmail.com>
2018-08-08 17:04:04 -0400

    Added some models

---
+++ EstimatingLibrary/TECToDoItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstimatingLibrary
+{
+    public class TECToDoItem : TECObject
+    {
+        private string _description = "";
+        private bool _isDone = false;
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    var old = _description;
+                    _description = value;
+                    notifyCombinedChanged(Change.Edit, "Description", this, value, old);
+                }
+            }
+        }
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (_isDone != value)
+                {
+                    _isDone = value;
+                    notifyCombinedChanged(Change.Edit, "IsDone", this, value, !value);
+                }
+            }
+        }
+
+        public TECToDoItem(Guid guid) : base(guid) { }
+    }
+}
---
+++ EstimatingLibrary/Utilities/ToDoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstimatingLibrary.Utilities
+{
+    public static class ToDoList
+    {
+        public static List<TECToDoItem> GetBidList()
+        {
+            List<TECToDoItem> list = new List<TECToDoItem>();
+
+        }
+    }
+}
